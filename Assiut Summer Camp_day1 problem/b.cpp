d2d37c767a88628c34aa8def1e2763158d555b30
MD. ROKONUZZAMAN <rokonkhan1819@gmail.com>
2025-07-23 19:56:02 +0600

    day 1 problem set

---
+++ Assiut Summer Camp_day1 problem/b.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int x;
+ long long lng;
+ char c;
+    float f;
+    double d;
+    cin>>x>>lng>>c>>f>>d;
+    cout<<x<<endl;
+    cout<<lng<<endl;
+    cout<<c<<endl;
+    cout<<f<<endl;
+    cout<<d<<endl;
+    return 0;
+}
