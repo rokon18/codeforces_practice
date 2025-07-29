cc535ddf70c8d997ac1c019010e2e401e67d9f50
MD. ROKONUZZAMAN <rokonkhan1819@gmail.com>
2025-07-30 01:17:10 +0600

    day 2 pb 1 all done

---
+++ Assiut Summer Camp_day2_problem_1/m.cpp
@@ -0,0 +1,30 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+   int N;
+   cin >> N;
+   switch (N)
+   {
+   case 61:
+       cout << "Art of computer programming" << endl;
+       break;
+  case 71:
+       cout << "Introduction to algorithm" << endl;
+       break;
+       case 11:
+       cout << "Competitive programming" << endl;
+       break;
+       case 29:
+       cout << "Programming challenges" << endl;
+       break;
+   default:
+       cout << "Not found" << endl;
+       break;
+   }
+
+
+   
+    return 0;
+}
