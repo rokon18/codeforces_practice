25b0c297d299a306f9f2fe772d6f35c9134083e4
MD. ROKONUZZAMAN <rokonkhan1819@gmail.com>
2025-08-09 21:36:35 +0600

    p2d2 d

---
+++ Assiut Summer Camp_day2_problem_2/d.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+   long long w;
+
+    cin >> w;
+
+if ((w%10)+((w/10)%10)<10){
+    cout << "EFTA7 YA SIMSIM" << endl;
+} else {
+    cout << "E2FEL YA SIMSIM" << endl;
+}
+    return 0;
+}
