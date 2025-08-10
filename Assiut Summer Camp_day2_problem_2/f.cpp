1117a4c1a1358e79a5cac437b194af49ff38f3cf
MD. ROKONUZZAMAN <rokonkhan1819@gmail.com>
2025-08-10 21:11:18 +0600

    f

---
+++ Assiut Summer Camp_day2_problem_2/f.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    if(n%4==0 && n%100!=0 || n%400==0){
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
